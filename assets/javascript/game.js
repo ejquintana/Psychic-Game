4ef789d6c2bb8b81355a1036d6540a4cc36008bb
Ed Quintana <ejquintana@gmail.com>
2017-08-06 17:24:56 -0600

    cleanup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,8 +8,9 @@ var guesses = 10;
 var guessesLeft = 10;
 var guessedLetters = [];
 var letterToGuess = "";
-var Wmsg = "Yes, you are psychic!"
-var lmsg = "I guess you're not a psyhic!"
+var Wmsg = ""
+var lmsg = ""
+
 
 // Using Math.floor to randomize computer selection from the Choices array
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
@@ -33,6 +34,8 @@ var updateGuessesSoFar = function() {
 document.onkeyup = function(event) {
     guessesLeft--;
   var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  var Wmsg = "Yes, you are psychic!"
+  var lmsg = "I guess you're not a psyhic!"
 
   guessedLetters.push(userGuess);
   updateGuessesLeft();
@@ -43,16 +46,12 @@ document.onkeyup = function(event) {
                 wins++;
                 document.querySelector('#wins').innerHTML = "Wins: " + wins;
                 document.querySelector('#msg').innerHTML = Wmsg;
-                // alert("Yes, you are psychic!");
-                reset();
             }
         }else if(guessesLeft == 0){
-            // Then we will loss and we'll update the html to display the loss 
             losses++;
             document.querySelector('#losses').innerHTML = "Losses: " + losses;
             document.querySelector('#msg').innerHTML = lmsg;
-            // alert("Sorry, you're not psychic, maybe try again?");
-            // Then we'll call the reset. ajd
+
             reset();
         }
 };
